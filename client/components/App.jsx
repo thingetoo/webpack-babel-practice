1e192e1b593271024c35efc8db6104023c8d7f0f
Brandon Galloway <brandon.galloway2@gmail.com>
2021-04-15 20:45:15 -0400

    Created a sample get request with axios

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import Cats from './Cats.jsx';
 import Overview from './Overview.jsx';
 import RelProductList from './RelProductList.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidMount() {
+
+    let headers = {
+      Authorization: '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'
+    }
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products', {headers})
+    .then(response => console.log(response.data));
+  }
+
+
+
   render() {
+
+
+
+
     return (
       <div>
         <h1>App.js is connected and working!</h1>
