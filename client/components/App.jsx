cb0378bf8a8d08eee37c62adb2dfe7b0d5cc7595
Joelchrist Abreu <ajoelcrist@gmail.com>
2021-04-16 10:35:09 -0400

    Added new component file, handles related products cards.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cats from './Cats.jsx';
 import RelProductList from './RelProductList.jsx';
+import TOKEN from '../../config.js';
 
 class App extends React.Component {
   constructor(props) {
