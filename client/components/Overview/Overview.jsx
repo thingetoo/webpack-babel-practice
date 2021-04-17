192219460d9e75c576c07bb7c9d7de69e3a0047e
Brandon Galloway <brandon.galloway2@gmail.com>
2021-04-17 10:29:33 -0400

    added tough functionality

--- client/components/Overview/Overview.jsx
+++ client/components/Overview/Overview.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import css from './Overview.css'
-import Thumbnail from './Thumbnail.jsx'
+import Thumbnail from './Thumbnail.jsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 class Overview extends React.Component{
   constructor(props) {
@@ -8,21 +10,34 @@ class Overview extends React.Component{
 
     console.log(props)
     this.state = {
-      thumbnails: ['img1', 'img2', 'img3', 'img4', 'img5']
+      thumbnails: ['img1', 'img2', 'img3', 'img4', 'img5', 'img6', 'img7'],
+      thumbnailsShown: [0, 5],
+    }
+    this.onArrowClick = this.onArrowClick.bind(this);
+  }
+
+  onArrowClick() {
+    if (this.state.thumbnailsShown[1] !== this.state.thumbnails.length) {
+      const diff = this.state.thumbnails.length - this.state.thumbnailsShown[1];
+      this.setState({
+        thumbnailsShown: [diff, this.state.thumbnailsShown[1] + diff]
+      })
     }
   }
 
   render() {
+    const { thumbnails, thumbnailsShown } = this.state;
     return this.props.product ?
     (
       <div>
         <div className='image-container'>
           <div className='image-container__thumbnail-container'>
           {
-            this.state.thumbnails.map((thumb, idx) => {
+            this.state.thumbnails.slice(thumbnailsShown[0], thumbnailsShown[1]).map((thumb, idx) => {
               return <Thumbnail thumb={thumb} key={idx} />
             })
           }
+          <FontAwesomeIcon onClick={() => this.onArrowClick()} icon={faArrowDown} />
           </div>
         </div>
       </div>
