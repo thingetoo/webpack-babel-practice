42133b3a805212eaa318addf9d56ce76eb8b3111
Joelchrist Abreu <ajoelcrist@gmail.com>
2021-04-19 12:10:30 -0400

    Created a request handler in server index to handle requests for related products, created a get request within the related product list component in client side, client is able to retrieve a list of products with its set of styles.

--- client/components/RelProductList.jsx
+++ client/components/RelProductList.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 import RelProductCard from './RelProductCard.jsx';
 
 class RelProductList extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rel_products: []
+    }
+  }
 
+  handleUpdate(id) {
+    axios.get(`/products/${id}/related`)
+      .then((response) => {
+        this.setState({
+          rel_products: response.data
+        })
+      })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.productId !== prevProps.productId) {
+      this.handleUpdate(this.props.productId);
+    }
+  }
+  componentDidMount() {
+
+  }
   render() {
     var arr = [1, 2, 3, 4, 5];
     // console.log(props.productId);
@@ -13,7 +34,7 @@ class RelProductList extends React.Component {
       <div>
         <h2>Related Products</h2>
         {
-          arr.map(() => {
+          this.state.rel_products.map(() => {
             return <RelProductCard />;
           })
         }
